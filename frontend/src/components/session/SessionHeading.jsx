7b9dd38f31d59c7194a0c54cf08cad07ba9cedbc
ornel77 <t.ornel@yahoo.fr>
2024-08-20 00:22:34 +0200

    small fix

--- frontend/src/components/session/SessionHeading.jsx
+++ frontend/src/components/session/SessionHeading.jsx
@@ -4,7 +4,7 @@ const SessionHeading = () => {
   return (
     <>
       <p className="text-xl font-bold mb-3">
-        <span className="text-red">Hi</span>,{" "}
+        Hi,
         <span className="italic">Adam</span>! 👋🏽{" "}
       </p>
       <h2 className="text-center text-2xl uppercase font-bold mb-4">
