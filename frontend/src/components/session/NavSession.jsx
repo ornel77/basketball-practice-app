b5f7d2cd4eedb9281113d2f386babe12391df079
ornel77 <ornella.tomboza@gmail.com>
2024-08-19 21:02:43 +0200

    fix navbar session

--- frontend/src/components/session/NavSession.jsx
+++ frontend/src/components/session/NavSession.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
+import FirstAngle from "./angles/FirstAngle";
+import ThirdAngle from "./angles/ThirdAngle";
+import SecondAngle from "./angles/SecondAngle";
+import FourthAngle from "./angles/FourthAngle";
+import FifthAngle from "./angles/FifthAngle";
 
 const NavSession = ({children}) => {
   const [activeTab, setActiveTab] = useState("first")
   return (
-    <nav className="flex justify-between gap-4">
-      <ul>
+    <nav className="">
+      <ul className="flex justify-between gap-4">
         <li>
           <button onClick={() => setActiveTab('first')}>0° Left</button>
         </li>
+        <li>
+          <button onClick={() => setActiveTab('second')}>45° Left</button>
+        </li>
+        <li>
+          <button onClick={() => setActiveTab('third')}>90°</button>
+        </li>
+        <li>
+          <button onClick={() => setActiveTab('fourth')}>45° RIGHT</button>
+        </li>
+        <li>
+          <button onClick={() => setActiveTab('fifth')}>0° RIGHT</button>
+        </li>
       </ul>
-      {children}
+      <div>
+        {activeTab === "first" && <FirstAngle /> }
+        {activeTab === "second" && <SecondAngle /> }
+        {activeTab === "third" && <ThirdAngle /> }
+        {activeTab === "fourth" && <FourthAngle /> }
+        {activeTab === "fifth" && <FifthAngle /> }
+      </div>
     </nav>
   );
 };
