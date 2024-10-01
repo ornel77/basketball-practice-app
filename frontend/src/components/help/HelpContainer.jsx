851d61accd05bd4b0b92b507662b0a2a899cc26b
ornel77 <t.ornel@yahoo.fr>
2024-10-01 11:06:42 +0200

    basic structure of help page

---
+++ frontend/src/components/help/HelpContainer.jsx
@@ -0,0 +1,23 @@
+const HelpContainer = () => {
+  return (
+    <>
+        <h2 className="title">Help</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque quaerat aliquid natus voluptate corrupti, quas cum facilis temporibus ipsa modi vel provident nisi possimus eligendi nihil minima aspernatur adipisci ex. Sapiente molestias id nobis excepturi dignissimos? Molestias quaerat quis eligendi, itaque cumque sit dignissimos esse odit nam eveniet asperiores voluptatum.
+        </p>
+        <br />
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque quaerat aliquid natus voluptate corrupti, quas cum facilis temporibus ipsa modi vel provident nisi possimus eligendi nihil minima aspernatur adipisci ex. Sapiente molestias id nobis excepturi dignissimos? Molestias quaerat quis eligendi, itaque cumque sit dignissimos esse odit nam eveniet asperiores voluptatum.
+        </p>
+        <br />
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque quaerat aliquid natus voluptate corrupti, quas cum facilis temporibus ipsa modi vel provident nisi possimus eligendi nihil minima aspernatur adipisci ex. Sapiente molestias id nobis excepturi dignissimos? Molestias quaerat quis eligendi, itaque cumque sit dignissimos esse odit nam eveniet asperiores voluptatum.
+        </p>
+        <br />
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque quaerat aliquid natus voluptate corrupti, quas cum facilis temporibus ipsa modi vel provident nisi possimus eligendi nihil minima aspernatur adipisci ex. Sapiente molestias id nobis excepturi dignissimos? Molestias quaerat quis eligendi, itaque cumque sit dignissimos esse odit nam eveniet asperiores voluptatum.
+        </p>
+    </>
+  )
+}
+export default HelpContainer
