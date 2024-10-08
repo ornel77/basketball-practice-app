57c313e3ef372114a24434e400b9c67dfb01486b
ornel77 <ornella.tomboza@gmail.com>
2024-10-08 14:40:43 +0200

    rien

--- frontend/src/components/historic/HistoricContainer.jsx
+++ frontend/src/components/historic/HistoricContainer.jsx
@@ -3,21 +3,23 @@ import { STATS } from "../../utils/data";
 import HistoricCard from "./HistoricCard";
 
 const HistoricContainer = () => {
-  const sortedStat = STATS.sort((a,b) => {
-    const dateA = new Date(a.sessionDate)
-    const dateB = new Date(b.sessionDate)
-    return dateB - dateA
-  })
- 
+  const sortedStat = STATS.sort((a, b) => {
+    const dateA = new Date(a.sessionDate);
+    const dateB = new Date(b.sessionDate);
+    return dateB - dateA;
+  });
+
   return (
     <>
       <h2 className="title">Historic</h2>
-      
-      <div className="mt-10
-      ">
-      {sortedStat.map((stat, i) => (
-        <HistoricCard stat={stat} key={i} />
-      ))}
+
+      <div
+        className="mt-10
+      "
+      >
+        {sortedStat.map((stat, i) => (
+          <HistoricCard stat={stat} key={i} />
+        ))}
       </div>
     </>
   );
