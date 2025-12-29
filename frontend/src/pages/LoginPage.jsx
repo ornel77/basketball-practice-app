c6449577aa8121fea44dbca95b4fa0811d6e18b8
ornel77 <ornella.tomboza@gmail.com>
2025-12-29 17:24:54 +0100

    implement ui for Login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,11 @@
+import LoginContainer from "../components/login/LoginContainer"
+
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className="gradient pt-6 text-slate-800 h-screen flex justify-center items-center">
+      <LoginContainer />
+    </div>
+  
   )
 }
 export default LoginPage
