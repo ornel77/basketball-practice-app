20e7fa6fad734cb310244d761077feca897fd909
ornel77 <ornella.tomboza@gmail.com>
2026-01-05 10:48:21 +0100

    animate welcome button with GSAP

--- frontend/src/components/welcome/WelcomeContainer.jsx
+++ frontend/src/components/welcome/WelcomeContainer.jsx
@@ -1,24 +1,53 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../../store/useAuthStore";
-
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const WelcomeContainer = () => {
-  const {authUser} = useAuthStore()
-  const navigate = useNavigate()
+  const { authUser } = useAuthStore();
+  const navigate = useNavigate();
   const handleEnterHomepage = () => {
-    if(authUser) {
-      navigate('/homepage')
+    if (authUser) {
+      navigate("/homepage");
     } else {
-      navigate("/login")
+      navigate("/login");
     }
-  }
+  };
+
+  useGSAP(() => {
+    const tl = gsap.timeline({repeat:-1, yoyo: true})
+    tl.fromTo(
+      "#btn",
+      {
+        // scale: 0.9,
+        y: -20,
+      },
+      {
+        // scale: 1,
+        y: 0,
+
+        repeat: 8,
+        yoyo: true,
+        ease: "power1.inOut",
+        duration: .7
+      }
+    ).to({y: 0}, {duration: 2})
+  }, []);
   return (
     <div className="bg-welcome">
       <div className="container flex flex-col justify-between h-screen items-center py-16">
         <div className="">
-          <img src="/welcome_title.png" alt="welcome-title" className="w-full" />
+          <img
+            src="/welcome_title.png"
+            alt="welcome-title"
+            className="w-full"
+          />
         </div>
-        <button className=" cursor-pointer" onClick={() => handleEnterHomepage()}>
+        <button
+          id="btn"
+          className="cursor-pointer"
+          onClick={() => handleEnterHomepage()}
+        >
           <img src="/enter_btn.png" alt="enter_btn" className="w-full" />
         </button>
       </div>
